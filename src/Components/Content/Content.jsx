de18f3e0f55c99eba40474eba1b2b08e69c18032
yuchenQ <qiaoycmelbourne@gmail.com>
2022-02-17 20:01:06 +1100

    feat: build layout for content

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './Content.module.css';
 
 const Content = () => {
   return (
-    <main>
-      content
+    <main className={styles.content}>
+      <section className={styles.layout}>
+        <div className={styles.tagLine}>
+          A better way
+          <br />
+          to enjoy every day
+        </div>
+        <div className={styles.secondTitle}>
+          Be the first to know when we launch.
+        </div>
+        <div>modal</div>
+      </section>
     </main>
   );
 };
