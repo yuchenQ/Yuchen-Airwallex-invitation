a2c3af15d7bacebd9f343a3dce7442b422730df0
yuchenQ <qiaoycmelbourne@gmail.com>
2022-02-17 22:16:47 +1100

    refactor: add propTypes for Page

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Page.module.css';
 
-const Page = ({ children }) => {
+const Page = ({ children, dataTestId }) => {
   return (
-    <article className={styles.page}>
+    <article className={styles.page} data-testid={dataTestId}>
       {children}
     </article>
   );
+};
+
+Page.defaultProps = {
+  dataTestId: '',
+};
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  dataTestId: PropTypes.string,
 };
 
 export { Page };
